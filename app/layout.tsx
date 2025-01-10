16d67b819b7cc0e60a1263981c89c18242049309
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-10 16:44:37 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ function rank() {
 	"author": {
             "@type": "Person",
             "name": "S8toto : Slot Deposit Pulsa Tanpa Potongan Dan Slot Deposit 5000",
-            "reviewBody": "S8toto : Slot Deposit Pulsa Tanpa Potongan Dan Slot Deposit 5000, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!"
+            "reviewBody": "S8toto hadir dengan berbagai kemudahan bagi para pemain slot online, salah satunya adalah pilihan Slot Deposit Pulsa Tanpa Potongan. Dengan fitur ini, pemain dapat melakukan deposit menggunakan pulsa tanpa harus khawatir akan adanya potongan. Ini adalah solusi yang praktis bagi mereka yang ingin bermain dengan mudah dan cepat"
           }
         },
     "offers": {
@@ -47,6 +47,22 @@ function rank() {
     };
   }
 
+function histats() {
+    return {
+      __html: `
+        var _Hasync= _Hasync|| [];
+        _Hasync.push(['Histats.start', '1,4921027,4,5,172,25,00010000']);
+        _Hasync.push(['Histats.fasi', '1']);
+        _Hasync.push(['Histats.track_hits', '']);
+        (function() {
+        var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+        hs.src = ('//s10.histats.com/js15_as.js');
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+        })();
+      `,
+    };
+  };
+
 export default function RootLayout({
   children
 }: Readonly<{
@@ -55,7 +71,6 @@ export default function RootLayout({
   return (
     <html lang="id" data-browser="chrome">
       <head>
-        <meta charSet="utf-8" />
         <title>S8toto : Slot Deposit Pulsa Tanpa Potongan Dan Slot Deposit 5000</title>
         <meta name="description" content="S8toto hadir dengan berbagai kemudahan bagi para pemain slot online, salah satunya adalah pilihan Slot Deposit Pulsa Tanpa Potongan. Dengan fitur ini, pemain dapat melakukan deposit menggunakan pulsa tanpa harus khawatir akan adanya potongan. Ini adalah solusi yang praktis bagi mereka yang ingin bermain dengan mudah dan cepat" />
         <meta name="keywords" content="S8toto, Slot Deposit Pulsa Tanpa Potongan, Slot Deposit 5000, Slot Deposit Pulsa, Slot Paling Gacor 2025, Situs Slot Terbaik 2025, Link Slot Gates OF Olympus, Situs Slot Daftar Dapat Bonus, Link Slot Bonus 100, Link Slot Baru Daftar Dapat Bonus" />
@@ -80,7 +95,7 @@ export default function RootLayout({
         <meta name="distribution" content="global" />
         <meta name="format-detection" content="telephone=no, address=no, email=no" />
         <meta property="og:title" content="S8toto : Slot Deposit Pulsa Tanpa Potongan Dan Slot Deposit 5000" />
-        <meta property="og:description" content="S8toto : Slot Deposit Pulsa Tanpa Potongan Dan Slot Deposit 5000, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
+        <meta property="og:description" content="S8toto hadir dengan berbagai kemudahan bagi para pemain slot online, salah satunya adalah pilihan Slot Deposit Pulsa Tanpa Potongan. Dengan fitur ini, pemain dapat melakukan deposit menggunakan pulsa tanpa harus khawatir akan adanya potongan. Ini adalah solusi yang praktis bagi mereka yang ingin bermain dengan mudah dan cepat" />
         <meta property="og:url" content={nameDomain} />
         <meta property="og:site_name" content="s8toto" />
         <meta property="og:locale" content="id_ID" />
@@ -91,12 +106,12 @@ export default function RootLayout({
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:creator" content="s8toto" />
         <meta name="twitter:title" content="S8toto : Slot Deposit Pulsa Tanpa Potongan Dan Slot Deposit 5000" />
-        <meta name="twitter:description" content="S8toto : Slot Deposit Pulsa Tanpa Potongan Dan Slot Deposit 5000, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
+        <meta name="twitter:description" content="S8toto hadir dengan berbagai kemudahan bagi para pemain slot online, salah satunya adalah pilihan Slot Deposit Pulsa Tanpa Potongan. Dengan fitur ini, pemain dapat melakukan deposit menggunakan pulsa tanpa harus khawatir akan adanya potongan. Ini adalah solusi yang praktis bagi mereka yang ingin bermain dengan mudah dan cepat" />
         <meta name="twitter:image" content={nameDomain} />
         <link rel="icon" href="/favicon.ico?favicon.088e6c96.ico" sizes="256x256" type="image/x-icon" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="next-size-adjust" content="" />
         <script type="application/ld+json" dangerouslySetInnerHTML={rank()} key="product-jsonld" />
+        <script dangerouslySetInnerHTML={histats()} type="text/javascript" />
       </head>
       <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
